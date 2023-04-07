aaf2c2fb0ea84db5133a813d820a3aecc0ba869e
Jutsz <judit.fulmer@gmail.com>
2023-04-07 11:12:58 +0200

    add ingredientlist to recipe entity

--- src/main/java/com/example/recipes/model/Recipe.java
+++ src/main/java/com/example/recipes/model/Recipe.java
@@ -1,15 +1,18 @@
 package com.example.recipes.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Recipe {
 
     @Id
@@ -19,6 +22,8 @@ public class Recipe {
     private String name;
     private RecipeType recipeType;
     private String text;
+    @ManyToMany
+    private List<Ingredient> ingredients;
 
 
 
