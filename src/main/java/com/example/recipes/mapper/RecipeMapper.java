3abde2d753a5c5c44be98bee86d40853af586471
Jutsz <judit.fulmer@gmail.com>
2023-04-07 17:04:05 +0200

    implement RecipeMapper

---
+++ src/main/java/com/example/recipes/mapper/RecipeMapper.java
@@ -0,0 +1,24 @@
+package com.example.recipes.mapper;
+
+import com.example.recipes.model.Ingredient;
+import com.example.recipes.model.Recipe;
+import com.example.recipes.model.dto.RecipeDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class RecipeMapper {
+    public RecipeDTO toDTO (Recipe recipe){
+        if (recipe==null){
+            return null;
+        }
+        return RecipeDTO.builder()
+                .id(recipe.getId())
+                .name(recipe.getName())
+                .recipeType(recipe.getRecipeType())
+                .text(recipe.getText())
+                .ingredientNames(recipe.getIngredients().stream().map(Ingredient::getName).collect(Collectors.toSet()))
+                .build();
+    }
+}
