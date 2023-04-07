71db65e5d00131d7468873d6c3e23712d49cb664
Jutsz <judit.fulmer@gmail.com>
2023-04-07 12:01:07 +0200

    add find recipe methods to RecipeDAO (not work properly)

--- src/main/java/com/example/recipes/repository/RecipeDAO.java
+++ src/main/java/com/example/recipes/repository/RecipeDAO.java
@@ -1,7 +1,13 @@
 package com.example.recipes.repository;
 
 import com.example.recipes.model.Recipe;
+import com.example.recipes.model.RecipeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeDAO extends JpaRepository<Recipe, Long> {
+    List<Recipe> findAllByIngredients_name(String ingredientName);
+
+    List<Recipe> findAllByRecipeTypeIsAndByIngredients_name(RecipeType recipeType, String ingredientName);
 }
