3e655a3d13df1bc3a287d0c8641c7ee9f5b68105
Jutsz <judit.fulmer@gmail.com>
2023-04-09 16:25:18 +0200

    add findAllByNameContainsIgnoreCase to IngredientDAO

--- src/main/java/com/example/recipes/repository/IngredientDAO.java
+++ src/main/java/com/example/recipes/repository/IngredientDAO.java
@@ -3,5 +3,9 @@ package com.example.recipes.repository;
 import com.example.recipes.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IngredientDAO extends JpaRepository<Ingredient, Long> {
+
+    List<Ingredient> findAllByNameContainsIgnoreCase(String name);
 }
