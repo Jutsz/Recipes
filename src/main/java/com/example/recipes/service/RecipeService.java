ca55bfdb5f253f1ae9d3c350e1256708bc2c4db2
Jutsz <judit.fulmer@gmail.com>
2023-04-10 16:15:21 +0200

    make connection with ingredients in addnewrecipe method in RecipeService

--- src/main/java/com/example/recipes/service/RecipeService.java
+++ src/main/java/com/example/recipes/service/RecipeService.java
@@ -1,22 +1,28 @@
 package com.example.recipes.service;
 
 import com.example.recipes.mapper.RecipeMapper;
+import com.example.recipes.model.Ingredient;
 import com.example.recipes.model.Recipe;
 import com.example.recipes.model.dto.RecipeDTO;
 import com.example.recipes.model.types.RecipeType;
+import com.example.recipes.repository.IngredientDAO;
 import com.example.recipes.repository.RecipeDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class RecipeService {
     private final RecipeDAO recipeDAO;
     private final RecipeMapper recipeMapper;
+    private final IngredientDAO ingredientDAO;
 
-    public RecipeService(RecipeDAO recipeDAO, RecipeMapper recipeMapper) {
+    public RecipeService(RecipeDAO recipeDAO, RecipeMapper recipeMapper, IngredientDAO ingredientDAO) {
         this.recipeDAO = recipeDAO;
         this.recipeMapper = recipeMapper;
+        this.ingredientDAO = ingredientDAO;
     }
 
     public List<RecipeDTO> getAllRecipes(){
@@ -42,13 +48,24 @@ public class RecipeService {
         return createDTOFromRecipeList(recipes);
     }
 
-    public void addNewRecipe(Recipe recipe){
+    public void addNewRecipe(RecipeDTO recipeDTO) {
+        Recipe recipe = new Recipe();
+        recipe.setName(recipeDTO.getName());
+        Set<Ingredient> ingredientsOfRecipe = new HashSet<>();
+        if (!recipeDTO.getIngredientNames().isEmpty()) {
+            for (String ingredientName : recipeDTO.getIngredientNames()) {
+                ingredientsOfRecipe.add(ingredientDAO.findByNameContainsIgnoreCase(ingredientName));
+            }
+        }
+        recipe.setRecipeType(recipeDTO.getRecipeType());
+        recipe.setText(recipeDTO.getText());
+        recipe.setIngredients(ingredientsOfRecipe);
         recipeDAO.save(recipe);
     }
-    /*TODO make the connection with ingredients*/
-
-    public void updateRecipe (Long id, Recipe recipe){
-        Recipe recipeToUpdate = recipeDAO.findById(id).orElseThrow();
+    public void updateRecipe (Long id, RecipeDTO recipeDTO){
+        Recipe recipeToUpdate = recipeDAO.findById(id).orElse(null);
+        Recipe recipe = new Recipe();
+        assert recipeToUpdate != null;
         recipe.setId(recipeToUpdate.getId());
         recipeDAO.save(recipe);
     }
