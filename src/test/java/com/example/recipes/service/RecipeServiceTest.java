5871b2ab3f47c3589adfee88a854bbc6988f44b9
Jutsz <judit.fulmer@gmail.com>
2023-04-27 11:12:11 +0200

    start implement RecipeServiceTest

--- src/test/java/com/example/recipes/service/RecipeServiceTest.java
+++ src/test/java/com/example/recipes/service/RecipeServiceTest.java
@@ -4,6 +4,7 @@ import com.example.recipes.mapper.RecipeMapper;
 import com.example.recipes.model.dto.RecipeDTO;
 import com.example.recipes.repository.IngredientDAO;
 import com.example.recipes.repository.RecipeDAO;
+import com.example.recipes.testmodels.Recipe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.recipes.data.TestRecipes.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RecipeServiceTest {
@@ -28,13 +33,16 @@ class RecipeServiceTest {
     @InjectMocks
     RecipeService recipeService;
 
+    private static List<Recipe> recipeList = new ArrayList<>();
+
     @BeforeEach
     void setUp() {
+        recipeList = List.of(rizseshús, brokkolisTészta, sajtosSzendvics, gyümölcsösJoghurt, korpovitCottage, csirkemellesSaláta);
     }
 
     @Test
     void shouldReturnAllRecipesWhenGetAllRecipes() {
-        List<RecipeDTO> expectedRecipes;
+        when(recipeDAO.findAll()).thenReturn(new ArrayList<>());/*TODO create the mocking*/
     }
 
     @Test
