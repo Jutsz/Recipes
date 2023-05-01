c406dcf26c5932e4703c8e60eda4160bf32d2e9c
Jutsz <judit.fulmer@gmail.com>
2023-05-01 16:12:19 +0200

    add tostring to recpe entity

--- src/main/java/com/example/recipes/model/Recipe.java
+++ src/main/java/com/example/recipes/model/Recipe.java
@@ -3,10 +3,7 @@ package com.example.recipes.model;
 import com.example.recipes.model.types.RecipeType;
 import jakarta.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Set;
 
@@ -15,6 +12,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Recipe {
 
     @Id
