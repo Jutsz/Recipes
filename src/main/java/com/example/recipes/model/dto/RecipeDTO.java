3650db4084b7e5815e04e13de283c43e8453a3c9
Jutsz <judit.fulmer@gmail.com>
2023-05-01 16:13:03 +0200

    add tostring, equals and hashcode to to RecipeDTO

--- src/main/java/com/example/recipes/model/dto/RecipeDTO.java
+++ src/main/java/com/example/recipes/model/dto/RecipeDTO.java
@@ -5,12 +5,14 @@ import lombok.*;
 import jakarta.validation.constraints.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
+@ToString
 public class RecipeDTO {
     private Long id;
     @NotEmpty
@@ -21,4 +23,17 @@ public class RecipeDTO {
     private String text;
     @NotEmpty
     private List<String> ingredientNames;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecipeDTO recipeDTO = (RecipeDTO) o;
+        return Objects.equals(id, recipeDTO.id) && Objects.equals(name, recipeDTO.name) && recipeType == recipeDTO.recipeType && Objects.equals(text, recipeDTO.text) && Objects.equals(ingredientNames, recipeDTO.ingredientNames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, recipeType, text, ingredientNames);
+    }
 }
