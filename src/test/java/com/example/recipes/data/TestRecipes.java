32ef1f8daaa03d72e62da694760c5cb8322c993b
Jutsz <judit.fulmer@gmail.com>
2023-05-01 16:14:52 +0200

    add id-s to TestRecipes

--- src/test/java/com/example/recipes/data/TestRecipes.java
+++ src/test/java/com/example/recipes/data/TestRecipes.java
@@ -6,17 +6,17 @@ import com.example.recipes.testmodels.RecipeType;
 import java.util.List;
 
 public interface TestRecipes {
-    Recipe rizseshús = new Recipe(null, "rizseshús", RecipeType.EBÉD, "készítsünk rizseshúst",
+    Recipe rizseshús = new Recipe(1L, "rizseshús", RecipeType.EBÉD, "készítsünk rizseshúst",
             List.of("olaj", "hagyma", "pirospaprika", "csirkemell", "rizs"));
-    Recipe brokkolisTészta = new Recipe(null, "brokkolis tészta", RecipeType.EBÉD,
+    Recipe brokkolisTészta = new Recipe(2L, "brokkolis tészta", RecipeType.EBÉD,
             "készítsünk brokkolis tésztát", List.of("hagyma", "fokhagyma", "brokkoli", "tészta", "tejszín"));
-    Recipe sajtosSzendvics = new Recipe(null, "sajtos szendvics", RecipeType.REGGELI,
+    Recipe sajtosSzendvics = new Recipe(3L, "sajtos szendvics", RecipeType.REGGELI,
             "készítsünk sajtos szendvicset", List.of("teljes kíőrlésű kenyér", "margarin", "light sajt"));
-    Recipe gyümölcsösJoghurt = new Recipe(null, "gyümölcsös joghurt", RecipeType.TÍZORAI,
+    Recipe gyümölcsösJoghurt = new Recipe(4L, "gyümölcsös joghurt", RecipeType.TÍZORAI,
             "keverjük össze a joghurtot a gyümölccsel", List.of("gyümölcs", "light joghurt"));
-    Recipe korpovitCottage = new Recipe(null, "korpovit keksz cottage cheese-zel", RecipeType.UZSONNA,
+    Recipe korpovitCottage = new Recipe(5L, "korpovit keksz cottage cheese-zel", RecipeType.UZSONNA,
             "4 db keksz, 100 g cottage", List.of("korpovit keksz", "cottage"));
-    Recipe csirkemellesSaláta = new Recipe(null, "csirkemelles saláta", RecipeType.VACSORA,
+    Recipe csirkemellesSaláta = new Recipe(6L, "csirkemelles saláta", RecipeType.VACSORA,
             "a csirkemellet megsütjük és összekeverjük a salátával",
             List.of("csirkemell, salátakeverék, teljes kiőrlésű kenyér"));
 
