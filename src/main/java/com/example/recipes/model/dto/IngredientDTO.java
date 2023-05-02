62b6988060427e309ffaa9862d22c595616b9164
Jutsz <judit.fulmer@gmail.com>
2023-05-02 23:47:02 +0200

    add equals and hashcode to IngredientDTO

--- src/main/java/com/example/recipes/model/dto/IngredientDTO.java
+++ src/main/java/com/example/recipes/model/dto/IngredientDTO.java
@@ -3,6 +3,7 @@ package com.example.recipes.model.dto;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -16,4 +17,17 @@ public class IngredientDTO {
     @NotEmpty
     private String name;
     private Set<String> recipeNames;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IngredientDTO that = (IngredientDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(recipeNames, that.recipeNames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, recipeNames);
+    }
 }
