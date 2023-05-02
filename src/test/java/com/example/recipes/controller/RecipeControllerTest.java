d01d29749ac1d4f0a6a86f083f05094b75b60f9f
Jutsz <judit.fulmer@gmail.com>
2023-05-02 23:48:20 +0200

    add RecipeControllerTest

---
+++ src/test/java/com/example/recipes/controller/RecipeControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.recipes.controller;
+
+import org.junit.jupiter.api.Test;
+
+class RecipeControllerTest {
+
+    @Test
+    void getAllRecipes() {
+    }
+
+    @Test
+    void getRecipeById() {
+    }
+
+    @Test
+    void getRecipeByIngredient() {
+    }
+
+    @Test
+    void getRecipesWithRecipeTypeByIngredient() {
+    }
+
+    @Test
+    void addNewRecipe() {
+    }
+
+    @Test
+    void updateRecipe() {
+    }
+
+    @Test
+    void deleteRecipe() {
+    }
+}
