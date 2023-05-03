9afe161ed712498a46cd315756781f3c48696e2f
Jutsz <judit.fulmer@gmail.com>
2023-05-03 10:50:20 +0200

    implement RecipeControllerTest - test coverage is 100% for service layer, 94% of controller layer

--- src/test/java/com/example/recipes/controller/RecipeControllerTest.java
+++ src/test/java/com/example/recipes/controller/RecipeControllerTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -40,21 +43,131 @@ class RecipeControllerTest {
 
     @Test
     void getRecipeByIngredientShouldReturnRecipes() {
+        String ingredientToSearch = "margarin";
+        RecipeDTO recipeDTO1 = new RecipeDTO(1L, "mozzarellás szendvics", RecipeType.REGGELI,
+                "készítsünk mozzarellás szendvicset", List.of("margarin", "teljes kiőrlésű kenyér",
+                "mozzarella"));
+        RecipeDTO recipeDTO2 = new RecipeDTO(2L, "sonkás szendvics", RecipeType.REGGELI,
+                "készítsünk sonkás szendvicset", List.of("margarin", "teljes kiőrlésű kenyér",
+                "sonka"));
+        RecipeDTO recipeDTO3 = new RecipeDTO(3L, "camambertes szendvics", RecipeType.REGGELI,
+                "készítsünk camambertes szendvicset", List.of("margarin", "teljes kiőrlésű kenyér",
+                "camambert"));
+        List<RecipeDTO> expectedResponse = List.of(recipeDTO1, recipeDTO2, recipeDTO3);
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "/recipes/ingredient?ingredient={ingredient}",
+                String.class,
+                ingredientToSearch
+        );
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(Objects.requireNonNull(response.getBody()).contains(expectedResponse.get(0).getName()));
+        assertTrue(Objects.requireNonNull(response.getBody()).contains(expectedResponse.get(1).getName()));
+        assertTrue(Objects.requireNonNull(response.getBody()).contains(expectedResponse.get(2).getName()));
     }
 
     @Test
-    void getRecipesWithRecipeTypeByIngredient() {
+    void getRecipeByNonExistingIngredientShouldReturnNotFound() {
+        String ingredientToSearch = "krumpli";
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "/recipes/ingredient?ingredient={ingredient}",
+                String.class,
+                ingredientToSearch
+        );
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
-    void addNewRecipe() {
+    void getRecipesWithRecipeTypeByIngredientShouldReturnRecipes() {
+        String ingredientToSearch = "rizs";
+        String recipeTypeToSearch = "ebéd";
+        RecipeDTO expectedRecipeDTO = new RecipeDTO(4L, "rizseshús", RecipeType.EBÉD,
+                "készítsünk rizseshúst", List.of("hagyma", "olaj", "csirkemell", "rizs"));
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "/recipes/recipetype/ingredient?recipeType={recipeType}&ingredient={ingredient}",
+                String.class,
+                recipeTypeToSearch,
+                ingredientToSearch
+        );
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(Objects.requireNonNull(response.getBody()).contains(expectedRecipeDTO.getName()));
     }
 
     @Test
-    void updateRecipe() {
+    void getRecipesWithRecipeTypeByNonExistingIngredientShouldReturnBadRequest() {
+        String ingredientToSearch = "krumpli";
+        String recipeTypeToSearch = "ebéd";
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "/recipes/recipetype/ingredient?recipeType={recipeType}&ingredient={ingredient}",
+                String.class,
+                recipeTypeToSearch,
+                ingredientToSearch
+        );
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
-    void deleteRecipe() {
+    void testAddNewRecipe() {
+        RecipeDTO recipeDTO = new RecipeDTO();
+        String recipeName = "testRecipe";
+        String ingredientName = "testIngredient";
+        String text = "test text";
+        RecipeType recipeType = RecipeType.EBÉD;
+        recipeDTO.setIngredientNames(List.of(ingredientName));
+        recipeDTO.setName(recipeName);
+        recipeDTO.setText(text);
+        recipeDTO.setRecipeType(recipeType);
+        HttpEntity<RecipeDTO> request = new HttpEntity<>(recipeDTO);
+        ResponseEntity<String> responseForPost = restTemplate.postForEntity("/recipes", request, String.class);
+        assertEquals(HttpStatus.OK, responseForPost.getStatusCode());
+        ResponseEntity<String> responseForGet = restTemplate.getForEntity(
+                "/recipes/ingredient?ingredient={ingredient}",
+                String.class,
+                ingredientName
+        );
+        assertTrue(Objects.requireNonNull(responseForGet.getBody()).contains(ingredientName));
+    }
+
+    @Test
+    void testAddNewRecipeWithMissingValuesShouldReturnBadRequest() {
+        RecipeDTO recipeDTO = new RecipeDTO();
+        String recipeName = "testRecipe";
+        recipeDTO.setName(recipeName);
+        HttpEntity<RecipeDTO> request = new HttpEntity<>(recipeDTO);
+        ResponseEntity<String> responseForPost = restTemplate.postForEntity("/recipes", request, String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, responseForPost.getStatusCode());
+    }
+
+    @Test
+    void testUpdateRecipe() {
+        RecipeDTO recipeDTO = new RecipeDTO();
+        String ingredientName = "testIngredient";
+        String text = "test text";
+        RecipeType recipeType = RecipeType.EBÉD;
+        recipeDTO.setIngredientNames(List.of(ingredientName));
+        recipeDTO.setText(text);
+        recipeDTO.setRecipeType(recipeType);
+        recipeDTO.setId(1L);
+        recipeDTO.setName("updatedRecipe");
+        HttpEntity<RecipeDTO> request = new HttpEntity<>(recipeDTO);
+        ResponseEntity<String> response = restTemplate.exchange("/recipes/update/1", HttpMethod.PUT, request, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        ResponseEntity<RecipeDTO> responseToGet = restTemplate.getForEntity("/recipes/1", RecipeDTO.class);
+        assertEquals("updatedRecipe", Objects.requireNonNull(responseToGet.getBody()).getName());
+    }
+
+    @Test
+    void testUpdateRecipeWithMissingValuesShouldReturnBadRequest() {
+        RecipeDTO recipeDTO = new RecipeDTO();
+        recipeDTO.setName("updatedRecipe");
+        HttpEntity<RecipeDTO> request = new HttpEntity<>(recipeDTO);
+        ResponseEntity<String> response = restTemplate.exchange("/recipes/update/1", HttpMethod.PUT, request, String.class);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void deletedRecipeShouldNotGetById() {
+        restTemplate.delete("/recipes/delete/1");
+        ResponseEntity<RecipeDTO> response = restTemplate.getForEntity("/recipes/1", RecipeDTO.class);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
