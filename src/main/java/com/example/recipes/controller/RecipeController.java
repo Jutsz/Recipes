239ee23032c098947b1ee77a3f865ba0c6f86fea
Jutsz <judit.fulmer@gmail.com>
2023-05-03 11:42:34 +0200

    implement swagger with @Operation in RecipeController class

--- src/main/java/com/example/recipes/controller/RecipeController.java
+++ src/main/java/com/example/recipes/controller/RecipeController.java
@@ -3,6 +3,10 @@ package com.example.recipes.controller;
 import com.example.recipes.model.dto.RecipeDTO;
 import com.example.recipes.model.types.RecipeType;
 import com.example.recipes.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -22,17 +26,37 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @Operation(
+            summary = "Get all recipes of the cookbook",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a list of Recipe entities."),
+            }
+    )
     @GetMapping
     public List<RecipeDTO> getAllRecipes() {
         return recipeService.getAllRecipes();
     }
 
+    @Operation(
+            summary = "Get a recipe by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a recipe with a given id"),
+                    @ApiResponse(responseCode = "404", description = "Recipe by the given id not found.")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<RecipeDTO> getRecipeById(@PathVariable("id") Long id) {
         Optional<RecipeDTO> recipeDTO = recipeService.getRecipeById(id);
         return recipeDTO.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(
+            summary = "Get a recipe by an ingredient",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a recipe with a given ingredient"),
+                    @ApiResponse(responseCode = "404", description = "Recipe with the given id not found.")
+            }
+    )
     @GetMapping("/ingredient")
     public ResponseEntity<List<RecipeDTO>> getRecipeByIngredient(@RequestParam(name = "ingredient") String ingredientName) {
         List<RecipeDTO> recipeDTOList = recipeService.getRecipeByIngredient(ingredientName);
@@ -43,6 +67,13 @@ public class RecipeController {
         }
     }
 
+    @Operation(
+            summary = "Get a recipe by an ingredient, in a given recipe type",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Responds with a recipe in the given recipe type, with the given ingredient"),
+                    @ApiResponse(responseCode = "404", description = "Recipe with the given ingredient and recipe type not found.")
+            }
+    )
     @GetMapping("/recipetype/ingredient")
     public ResponseEntity<List<RecipeDTO>> getRecipesWithRecipeTypeByIngredient(@RequestParam(name = "recipeType") String recipeType,
                                                                 @RequestParam(name = "ingredient") String ingredientName) {
@@ -53,6 +84,15 @@ public class RecipeController {
         } else return ResponseEntity.ok(recipeDTOList);
     }
 
+    @Operation(
+            summary = "Add a new recipe to the cookbook",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The recipe has been added to the cookbook.",
+                            content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RecipeDTO.class)) }),
+                    @ApiResponse(responseCode = "400", description = "The recipe was tried with bad values added.")
+            }
+    )
     @PostMapping
     public ResponseEntity<String> addNewRecipe(@RequestBody @Valid RecipeDTO recipeDTO, BindingResult errors) {
         if (errors.hasErrors()) return ResponseEntity.badRequest().build();
@@ -62,6 +102,15 @@ public class RecipeController {
         }
     }
 
+    @Operation(
+            summary = "Update a recipe in the cookbook wih a given id.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The recipe has been updated in the cookbook.",
+                            content = { @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = RecipeDTO.class)) }),
+                    @ApiResponse(responseCode = "400", description = "The recipe was tried with bad values updated.")
+            }
+    )
     @PutMapping("/update/{id}")
     public ResponseEntity<String> updateRecipe(@PathVariable Long id, @RequestBody @Valid RecipeDTO recipeDTO) {
         if (Objects.equals(id, recipeDTO.getId()) && recipeService.updateRecipe(id, recipeDTO).isPresent()) {
@@ -71,6 +120,15 @@ public class RecipeController {
         }
     }
 
+    @Operation(
+            summary = "Delete a recipe in the cookbook with a given id.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The recipe has been deleted from the cookbook.",
+                            content = { @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = RecipeDTO.class)) }),
+                    @ApiResponse(responseCode = "500", description = "There is no recipe with the given id in the cookbook.")
+            }
+    )
     @DeleteMapping("/delete/{id}")
     public void deleteRecipe(@PathVariable Long id) {
         recipeService.deleteRecipe(id);
